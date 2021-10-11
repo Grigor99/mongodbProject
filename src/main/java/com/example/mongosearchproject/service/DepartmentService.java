d6af13b6eb94205e5d9ac51850e74177c08799c0
grigormartirosyan <grigormartirosyan700@gmail.com>
2021-10-11 11:20:10 +0400

    service change

--- src/main/java/com/example/mongosearchproject/service/DepartmentService.java
+++ src/main/java/com/example/mongosearchproject/service/DepartmentService.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 public interface DepartmentService {
 
-    @ChangeSet(order = "001", id = "department_address_id", author = "dai")
     void createDepartment(DepartmentAddressDto dto);
 
     void createEmployee(EmployeeDto dto, String depId);
